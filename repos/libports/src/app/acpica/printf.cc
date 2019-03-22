67b6b8e3da837bd23cdb0eb542a50167caa8d443
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2019-03-22 15:22:16 +0000

    DEBUG - acpica printf

--- repos/libports/src/app/acpica/printf.cc
+++ repos/libports/src/app/acpica/printf.cc
@@ -7,11 +7,38 @@
 
 #include <stdarg.h>
 
-extern "C"
-void AcpiOsPrintf (const char *fmt, ...)
-{ }
+#include <base/snprintf.h>
+#include <log_session/log_session.h>
+
+void vprintf(const char *format, va_list &args)
+{
+	using namespace Genode;
+
+	static char buf[Log_session::MAX_STRING_LEN-4];
+
+	String_console sc(buf, sizeof(buf));
+	sc.vprintf(format, args);
+
+	int n = sc.len();
+	if (0 < n && buf[n-1] == '\n') n--;
+
+	log("VMM: ", Cstring(buf, n));
+}
 
 extern "C"
-void AcpiOsVprintf (const char *fmt, va_list va)
-{ }
+void AcpiOsPrintf (const char *format, ...)
+{
+	va_list list;
+	va_start(list, format);
+
+	::vprintf(format, list);
+
+	va_end(list);
+}
+
+extern "C"
+void AcpiOsVprintf (const char *format, va_list &va)
+{
+	::vprintf(format, va);
+}
 
