412639933a68d0504aa553e581f4b4d4ce2e8790
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2019-03-23 20:07:06 +0100

    Revert "DEBUG - acpica printf"
    This reverts commit 654cbdd4d011ef2233847219209e7f06d3b19375.

--- repos/libports/src/app/acpica/printf.cc
+++ repos/libports/src/app/acpica/printf.cc
@@ -7,38 +7,11 @@
 
 #include <stdarg.h>
 
-#include <base/snprintf.h>
-#include <log_session/log_session.h>
-
-void vprintf(const char *format, va_list &args)
-{
-	using namespace Genode;
-
-	static char buf[Log_session::MAX_STRING_LEN-4];
-
-	String_console sc(buf, sizeof(buf));
-	sc.vprintf(format, args);
-
-	int n = sc.len();
-	if (0 < n && buf[n-1] == '\n') n--;
-
-	log("VMM: ", Cstring(buf, n));
-}
+extern "C"
+void AcpiOsPrintf (const char *fmt, ...)
+{ }
 
 extern "C"
-void AcpiOsPrintf (const char *format, ...)
-{
-	va_list list;
-	va_start(list, format);
-
-	::vprintf(format, list);
-
-	va_end(list);
-}
-
-extern "C"
-void AcpiOsVprintf (const char *format, va_list &va)
-{
-	::vprintf(format, va);
-}
+void AcpiOsVprintf (const char *fmt, va_list va)
+{ }
 
