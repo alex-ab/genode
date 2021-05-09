62a352d23b8a39d6ce0dc0548e9dff1ee9a94fd5
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2021-05-09 15:33:04 +0200

    sculpt: add cpu resource support (tweak to x86)

--- repos/gems/src/app/sculpt_manager/runtime/nic_drv.cc
+++ repos/gems/src/app/sculpt_manager/runtime/nic_drv.cc
@@ -20,7 +20,7 @@ void Sculpt::gen_nic_drv_start_content(Xml_generator &xml)
 	                         Cap_quota{300}, Ram_quota{16*1024*1024},
 	                         Priority::NETWORK);
 
-	gen_named_node(xml, "resource", "CPU", [&] () { xml.attribute("quantum", "50"); });
+//	gen_named_node(xml, "resource", "CPU", [&] () { xml.attribute("quantum", "50"); });
 
 	xml.node("config", [&] () { });
 
