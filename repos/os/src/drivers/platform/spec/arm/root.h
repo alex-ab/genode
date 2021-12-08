a3405fd12568412a9e1ea6c022877a817883cf6c
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2021-12-08 14:56:46 +0100

    platform/arm: use imx8-scfw - WIP
    - eth0 pad config
    - irq enable
    - power on/off experiments of smmu, enet0

--- repos/os/src/drivers/platform/spec/arm/root.h
+++ repos/os/src/drivers/platform/spec/arm/root.h
@@ -21,6 +21,7 @@
 
 #include <device.h>
 #include <smmu.h>
+#include <scfw.h>
 
 namespace Driver { class Root; }
 
@@ -39,6 +40,7 @@ class Driver::Root : public Root_component<Session_component>
 		void _upgrade_session(Session_component *, const char *) override;
 
 		Driver::Env               & _env;
+		Driver::Scfw                _scfw { _env };
 		Constructible<Driver::Smmu> _smmu { };
 
 		Registry<Session_component> _sessions {};
--- repos/os/src/drivers/platform/spec/arm_64/target.mk
+++ repos/os/src/drivers/platform/spec/arm_64/target.mk
@@ -3,6 +3,6 @@ REQUIRES = arm_64
 SRC_CC   = device.cc device_component.cc device_model_policy.cc main.cc session_component.cc root.cc
 INC_DIR  = $(PRG_DIR)/../../spec/arm
 INC_DIR += $(BASE_DIR)/../base-hw/src/include
-LIBS     = base
+LIBS     = base imx8-scfw
 
 vpath %.cc $(PRG_DIR)/../../spec/arm
