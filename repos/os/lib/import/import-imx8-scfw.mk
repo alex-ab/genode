45ad2105c71ddb8e9cb87d0514fb6d251e4d5ae6
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2022-01-05 11:34:24 +0100

    Revert "imx8 scfw library support - WIP"
    This reverts commit 468b07a62d48c7b167e603e6142368f308252621.

--- repos/os/lib/import/import-imx8-scfw.mk
+++
@@ -1,5 +0,0 @@
-INC_BASE_DIR_SCFW = $(call select_from_ports,imx8-scfw)/src/lib/imx8-scfw
-INC_DIR += $(INC_BASE_DIR_SCFW)/arch/arm/include
-INC_DIR += $(INC_BASE_DIR_SCFW)/arch/arm/include/asm/mach-imx
-INC_DIR += $(INC_BASE_DIR_SCFW)/arch/arm/mach-imx
-INC_DIR += $(call select_from_repositories,src/lib/imx8-scfw)
--- repos/os/src/test/imx8-scfw/target.mk
+++
@@ -1,3 +0,0 @@
-TARGET = test-scfw
-SRC_CC = main.cc
-LIBS   = base imx8-scfw
