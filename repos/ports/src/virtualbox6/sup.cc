29e8ac3861c21f5b734373c413ec6344ca349961
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2024-09-04 10:52:49 +0200

    Revert "vbox6: enable required features for AVX* - WiP"
    This reverts commit 21821406a67082e7c25d9127297fde6d8362cdfd.

--- repos/ports/src/virtualbox6/sup.cc
+++ repos/ports/src/virtualbox6/sup.cc
@@ -138,12 +138,10 @@ static void ioctl(SUPVTCAPS &request)
 		switch (sup_drv->cpu_virt()) {
 		case Sup::Drv::Cpu_virt::VMX:
 			rc        = VERR_VMX_NO_VMX;
-			rc        = VINF_SUCCESS; //VERR_VMX_NO_VMX;
-			out.fCaps = SUPVTCAPS_VT_X | SUPVTCAPS_NESTED_PAGING |
-			            SUPVTCAPS_VTX_UNRESTRICTED_GUEST;
+			out.fCaps = SUPVTCAPS_VT_X | SUPVTCAPS_NESTED_PAGING;
 			break;
 		case Sup::Drv::Cpu_virt::SVM:
-			rc        = VINF_SUCCESS; //VERR_SVM_NO_SVM;
+			rc        = VERR_SVM_NO_SVM;
 			out.fCaps = SUPVTCAPS_AMD_V | SUPVTCAPS_NESTED_PAGING;
 			break;
 		case Sup::Drv::Cpu_virt::NONE:
