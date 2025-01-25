12c8ebebeb23fc14ade0895903c357ff37e94fd2
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2025-01-25 17:31:58 +0100

    sel4: Ram_allocator::Denied exception - DEBUG
    let survive entrypoint und pager_ep

--- repos/base-sel4/src/core/pager.cc
+++ repos/base-sel4/src/core/pager.cc
@@ -204,6 +204,7 @@ void Pager_entrypoint::entry()
 
 		reply_pending = false;
 
+	try {
 		Pool::apply(_pager.badge(), [&] (Pager_object *obj) {
 			if (!obj)
 				return;
@@ -232,5 +233,8 @@ void Pager_entrypoint::entry()
 			else
 				_pager.reply_save_caller(obj->reply_cap_sel());
 		});
+	} catch (...) {
+		error("uncatched exception in Pager_entrypoint");
+	}
 	}
 }
