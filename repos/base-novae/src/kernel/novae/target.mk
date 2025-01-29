b837555c53695eef33456636f4c191f381521a23
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2025-01-29 15:51:44 +0100

    novae: una estrella nueva

---
+++ repos/base-novae/src/kernel/novae/target.mk
@@ -0,0 +1,18 @@
+include $(REP_DIR)/etc/board.conf
+TARGET = novae-$(BOARD)
+LIBS   = kernel-novae-$(BOARD)
+
+ifneq ($(filter x86_64, $(SPECS)),)
+
+$(INSTALL_DIR)/$(TARGET):
+	ln -sf $(INSTALL_DIR)/../kernel/novae/x86_64-nova $@
+
+else
+
+$(INSTALL_DIR)/$(TARGET):
+	ln -sf $(INSTALL_DIR)/../kernel/novae/aarch64-$(BOARD)-nova $@
+
+endif
+
+clean cleanall:
+	$(VERBOSE)rm -rf $(PWD)/kernel/novae
