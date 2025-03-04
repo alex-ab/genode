7cb7776b99fd5d60e302a13f58ba63fb3a4940c9
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2025-03-04 09:33:42 +0100

    TMP - enable verbose quota trading in pager
    Issue #5455

--- repos/base-nova/src/core/pager.cc
+++ repos/base-nova/src/core/pager.cc
@@ -31,7 +31,7 @@
 #include <nova_util.h> /* map_local */
 #include <nova/capability_space.h>
 
-static bool verbose_oom = false;
+static bool verbose_oom = true;
 
 using namespace Core;
 using namespace Nova;
