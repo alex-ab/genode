5f5cc7883a349f7e2930dfb2d244475219413803
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2025-07-02 10:12:34 +0200

    sel4: add large page (super page) support - x86 only - WiP

--- repos/base-sel4/src/core/include/untyped_memory.h
+++ repos/base-sel4/src/core/include/untyped_memory.h
@@ -135,6 +135,39 @@ struct Core::Untyped_memory
 
 
 	/**
+	 * Create page frames from untyped memory
+	 */
+	static inline bool convert_to_large_page_frame(addr_t phys_addr)
+	{
+			seL4_Untyped const service     = Untyped_memory::_core_local_sel(Core_cspace::TOP_CNODE_UNTYPED_LARGE, phys_addr, 21).value();
+			seL4_Word    const type        = seL4_X86_LargePageObject;
+			seL4_Word    const size_bits   = 0;
+			seL4_CNode   const root        = Core_cspace::top_cnode_sel();
+			seL4_Word    const node_index  = Core_cspace::TOP_CNODE_PHYS_IDX;
+			seL4_Word    const node_depth  = Core_cspace::NUM_TOP_SEL_LOG2;
+			seL4_Word    const node_offset = phys_addr >> get_page_size_log2();
+			seL4_Word    const num_objects = 1;
+
+			long const ret = seL4_Untyped_Retype(service,
+			                                     type,
+			                                     size_bits,
+			                                     root,
+			                                     node_index,
+			                                     node_depth,
+			                                     node_offset,
+			                                     num_objects);
+
+			if (ret == seL4_NoError)
+				return true;
+
+			error(__FUNCTION__, ": seL4_Untyped_RetypeAtOffset "
+			      "returned ", ret);
+
+			return false;
+	}
+
+
+	/**
 	 * Free up page frames and turn it so into untyped memory
 	 */
 	static inline void convert_to_untyped_frames(addr_t const phys_addr,
