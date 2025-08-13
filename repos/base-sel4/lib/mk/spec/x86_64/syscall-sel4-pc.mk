8b5da2b9e8370ec6cc439fd3e107d1909ac279ee
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2025-08-13 10:42:43 +0200

    Revert "sel4: update to 13.0.0"
    This reverts commit 356e05f6ff3ff7ad74a50d1b50c6ff8e0c245f4d.

--- repos/base-sel4/lib/mk/spec/x86_64/syscall-sel4-pc.mk
+++ repos/base-sel4/lib/mk/spec/x86_64/syscall-sel4-pc.mk
@@ -5,7 +5,7 @@ BOARD := pc
 SEL4_ARCH := x86_64
 SEL4_WORDBITS := 64
 
-ARCH_INCLUDES := vmenter.h
+ARCH_INCLUDES := exIPC.h vmenter.h
 SEL4_ARCH_INCLUDES := syscalls_syscall.h
 
 LIBS += kernel-sel4-pc
