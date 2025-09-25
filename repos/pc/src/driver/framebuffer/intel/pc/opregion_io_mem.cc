bc72c1d613bcdbf27c161e671eb3cfafb63be67a
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2025-09-25 11:53:11 +0200

    intel/display: use intel_opregion device
    instead of requesting a copy of the region provided as ROM
    
    Issue #5705

--- repos/pc/src/driver/framebuffer/intel/pc/opregion_io_mem.cc
+++ repos/pc/src/driver/framebuffer/intel/pc/opregion_io_mem.cc
@@ -5,7 +5,7 @@
  */
 
 /*
- * Copyright (C) 2022 Genode Labs GmbH
+ * Copyright (C) 2022-2025 Genode Labs GmbH
  *
  * This file is distributed under the terms of the GNU General Public License
  * version 2.
@@ -14,62 +14,43 @@
 
 #include <base/log.h>
 #include <lx_kit/env.h>
-#include <base/attached_io_mem_dataspace.h>
-#include <base/attached_rom_dataspace.h>
-
-#include <region_map/client.h>
-#include <rm_session/connection.h>
-#include <rom_session/connection.h>
 
 #include <lx_emul.h>
+#include <lx_emul/io_mem.h>
 
 extern "C" void * intel_io_mem_map(unsigned long const phys,
                                    unsigned long const size)
 {
 	using namespace Genode;
 
-	static Constructible<Attached_rom_dataspace> rom_opregion { };
-	static addr_t opregion_start = 0;
-	static addr_t opregion_size  = 0;
+	static unsigned long opregion_phys = 0;
+	static unsigned long opregion_size = 0;
 
-	if (!rom_opregion.constructed()) {
-		try {
-			rom_opregion.construct(Lx_kit::env().env, "intel_opregion");
-
-			Dataspace_client ds_client(rom_opregion->cap());
-
-			auto mem_local = rom_opregion->local_addr<char>();
-			opregion_start = *(addr_t*)(mem_local + ds_client.size() - sizeof(addr_t) * 2);
-			opregion_size  = *(addr_t*)(mem_local + ds_client.size() - sizeof(addr_t));
-
-			if (opregion_size > ds_client.size())
-				opregion_size = ds_client.size();
-		} catch (...) {
-			error("Intel opregion ROM lookup failed");
-			if (rom_opregion.constructed())
-				rom_opregion.destruct();
-			return nullptr;
-		}
+	if (phys < OPREGION_PSEUDO_PHYS_ADDR || !size) {
+		warning("Unknown ", __func__, " range ", Hex_range(phys, size));
+		return nullptr;
 	}
+
+	if (!opregion_size)
+		Lx_kit::env().devices.for_each([&] (auto &d) {
+			if (d.name() != "intel_opregion")
+				return;
+
+			d.for_each_io_mem([&] (auto const &mem) {
+				opregion_phys = mem.addr;
+				opregion_size = mem.size;
+			});
+		});
 
 	/*
 	 * we have to substract the pseudo physical address
 	 * we returned when reading the ASLS from config space
 	 */
-	addr_t offset = phys - OPREGION_PSEUDO_PHYS_ADDR;
-	if ((offset + size) <= opregion_size) {
-
-		try {
-			auto ptr = ((addr_t)rom_opregion->local_addr<void>())
-			           + offset + (opregion_start & 0xffful);
-			return (void *)ptr;
-		} catch (...) {
-			error("Intel opregion lookup failed");
-			return nullptr;
-		}
+	addr_t const offset = phys - OPREGION_PSEUDO_PHYS_ADDR;
+	if ((offset + size) > opregion_size) {
+		warning("Unknown ", __func__, " range ", Hex_range(phys, size));
+		return nullptr;
 	}
 
-	warning("Unknown memremap range ", Hex_range(phys, size));
-
-	return nullptr;
+	return lx_emul_io_mem_map(opregion_phys + offset, opregion_size - offset);
 }
