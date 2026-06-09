720bd2b4252fcef20fc052ec87e0e7279dcdfdd8
Johannes Schlatow <johannes.schlatow@genode-labs.com>
2026-06-09 10:17:46 +0200

    linux_slirp_nic: drop dependency to host libslirp
    Instead of linking linux_slirp_nic against the host's libslirp, we
    add a port of libslirp and link against the statically built
    library.

---
+++ repos/libports/lib/import/import-libslirp.mk
@@ -0,0 +1,3 @@
+LIBSLIR_DIR := $(call select_from_ports,libslirp)
+INC_DIR += $(LIBSLIRP_DIR)/include
+INC_DIR += $(REP_DIR)/src/lib/libslirp/
---
+++ repos/libports/recipes/api/libslirp/content.mk
@@ -0,0 +1,30 @@
+MIRROR_FROM_REP_DIR := lib/mk/libslirp.mk \
+                       lib/import/import-libslirp.mk
+
+content: $(MIRROR_FROM_REP_DIR)
+
+$(MIRROR_FROM_REP_DIR):
+	$(mirror_from_rep_dir)
+
+PORT_DIR := $(call port_dir,$(REP_DIR)/ports/libslirp)
+MIRROR_FROM_PORT_DIR := include src/lib/libslirp/src
+
+content: $(addprefix include/slirp/,libslirp.h libslirp-version.h) $(MIRROR_FROM_PORT_DIR)
+
+$(MIRROR_FROM_PORT_DIR):
+	mkdir -p $(dir $@)
+	cp -r $(PORT_DIR)/$@ $@
+
+include/slirp/libslirp.h:
+	mkdir -p include/slirp
+	cp $(PORT_DIR)/include/slirp/libslirp.h include/slirp/libslirp.h
+
+include/slirp/libslirp-version.h:
+	mkdir -p include/slirp
+	cp $(REP_DIR)/src/lib/libslirp/libslirp-version.h include/slirp/libslirp-version.h
+
+content: LICENSE
+
+LICENSE:
+	cp $(PORT_DIR)/src/lib/libslirp/COPYRIGHT $@
+
