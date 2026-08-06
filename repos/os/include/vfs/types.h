1c89ff00dec3aa8adc1c8cba7c95190c937ba8c6
Norman Feske <norman.feske@genode-labs.com>
2026-08-06 14:32:35 +0200

    vfs: use 'Attempt' for 'Write_result'
    This patch prunes the number of possible write-error values and returns
    the number of written bytes as return value in the success case.
    
    Issue genodelabs/genode#4706

--- repos/os/include/vfs/types.h
+++ repos/os/include/vfs/types.h
@@ -67,6 +67,11 @@ namespace Genode::Vfs {
 		                                 .executable = true }; }
 	};
 
+	/**
+	 * Seek position in bytes, for read and write operations
+	 */
+	struct At { file_size offset; };
+
 	using Absolute_path = Path<MAX_PATH_LEN>;
 
 	struct Scanner_policy_path_element
@@ -104,8 +109,9 @@ namespace Genode::Vfs {
 
 	struct File_system_factory;
 
-	enum Write_result { WRITE_ERR_WOULD_BLOCK, WRITE_ERR_INVALID,
-	                    WRITE_ERR_IO,          WRITE_OK };
+	enum class Write_error { RETRY, DENIED };
+
+	using Write_result = Attempt<size_t, Write_error>;
 
 	enum class Read_error { RETRY, DENIED };
 
