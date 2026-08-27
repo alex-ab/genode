alex-ab/genode

commit 4aae41052e6e2eebafe68ee677cf815ff9f188c3

tool/port: honor CONTRIB_DIR in prepare_single_port.mk

tool/ports/mk/prepare_single_port.mk
@@ -4,12 +4,7 @@
 # \date   2014-05-07
 #
 
-#
-# Determine Genode base directory based on the known location of the
-# 'create_builddir' tool within the Genode source tree
-#
-export GENODE_DIR := $(realpath $(dir $(MAKEFILE_LIST))/../../..)
-export GENODE_CONTRIB_CACHE ?= $(GENODE_DIR)/contrib/cache
+export GENODE_CONTRIB_CACHE ?= $(CONTRIB_DIR)/contrib/cache
 
 include $(GENODE_DIR)/tool/ports/mk/front_end.inc
 include $(GENODE_DIR)/tool/ports/mk/check_port_arg.inc

tool/ports/prepare_port
@@ -44,4 +44,4 @@ $(CONTRIB_DIR)/.git/HEAD:
 	@$(VERBOSE)git init --quiet $(CONTRIB_DIR)
 
 $(TARGETS): $(CONTRIB_DIR)/.git/HEAD
-	@$(MAKE) -f $(GENODE_DIR)/tool/ports/mk/prepare_single_port.mk $@
+	@$(MAKE) -f $(GENODE_DIR)/tool/ports/mk/prepare_single_port.mk CONTRIB_DIR=$(CONTRIB_DIR) $@

commit 72cc15327f11c2b4c2d5b132d8cea9b382a5e5f5

tool/port: fix contrib/cache dir in prepare_single_port.mk
I broke this in commit

  4aae41052e6e2eeb tool/port: honor CONTRIB_DIR in prepare_single_port.mk

tool/ports/mk/prepare_single_port.mk
@@ -4,7 +4,7 @@
 # \date   2014-05-07
 #
 
-export GENODE_CONTRIB_CACHE ?= $(CONTRIB_DIR)/contrib/cache
+export GENODE_CONTRIB_CACHE ?= $(CONTRIB_DIR)/cache
 
 include $(GENODE_DIR)/tool/ports/mk/front_end.inc
 include $(GENODE_DIR)/tool/ports/mk/check_port_arg.inc